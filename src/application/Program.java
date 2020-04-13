3e5234e34d6426c12d97562e98a953f117e85bed
Gcf-droid <menezesgcf@gmail.com>
2020-04-13 15:06:08 -0300

    Estrutura finalizada

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,16 +8,25 @@ public class Program {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.println("How many tries ?");
-		System.out.println("Teste de upload para o Git Hub");
+		//System.out.println("Teste de upload para o Git Hub");
 		int x = sc.nextInt();
-		double[] rota = new double[x];
-		
+		System.out.println("Enter secret number");
+		int l = sc.nextInt();
+		int[] rota = new int[x];
+
 		for (int i = 0; i < rota.length; i++) {
 			System.out.println("Entry lucky number");
-			rota[i]=sc.nextDouble();
+			rota[i] = sc.nextInt();
 		}
-		for (int i=0;i<rota.length;i++) {
+		for (int i = 0; i < rota.length; i++) {
 			System.out.println(rota[i]);
+			if (rota[i] != l) {
+				System.out.println("Bad luck, try again !");
+			} else {
+
+				System.out.println("The victory is yours");
+				System.out.println("Number " + l);
+			}
 		}
 		sc.close();
 	}
